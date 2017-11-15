b63259ef8c6d32f0b796e940fea1a679719e0faa
karyna.avetsisian <avetisyan.k.workmail@gmail.com>
2017-11-15 18:48:06 +0300

    commit6 - методы масивов

---
+++ js/ex2.js
@@ -0,0 +1,21 @@
+/*Методы массивов*/
+
+/*1
+*Добавить класс в строку*/
+let obj = {
+    className: 'open menu'
+}
+
+function addClass(obj, cls) {
+    let arrayClass= obj.className.split(' ');
+    if ( arrayClass.indexOf( cls ) ){
+        arrayClass.push( cls );
+    }
+    obj.className = arrayClass.join(' ');
+}
+
+addClass(obj, 'new'); // obj.className='open menu new'
+addClass(obj, 'open'); // без изменений (класс уже существует)
+addClass(obj, 'me'); // obj.className='open menu new me'
+
+alert( obj.className );
