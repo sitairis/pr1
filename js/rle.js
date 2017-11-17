b796a4ef623bba3c8a0087d7ff88d35ed7acfd6b
karyna.avetsisian <avetisyan.k.workmail@gmail.com>
2017-11-17 17:16:38 +0300

    rle сжатие ГОТОВО

--- js/rle.js
+++ js/rle.js
@@ -4,33 +4,38 @@ function rleCompression( str ) {
     let array = str.split('');
     let resultArray = [];
     let count = 1;
-
-    resultArray = array.map( ( item ) => {
-        if ( item ){
-            return count++;
+    for (let i = 0, j = 1; i < array.length; i++, j++ ){
+        if ( array[i] != array[j] && count > 1 ){
+            resultArray.push( array[i], count );
+            count = 1;
+        } else {
+            if ( array[i] != array[j] && count == 1 ){
+                resultArray.push( array[i] );
+            }
+            count++;
         }
-        return count
-    });
-
-    // for (let i = 0, j = 1; i < array.length; i++, j++ ){
-    //     // if ( array[i] == array[j] ){
-    //     //     count++;
-    //     // } else {
-    //     //     if ( count == 1){
-    //     //         resultArray.push( array[i] );
-    //     //     } else {
-    //     //         resultArray.push( array[i], count );
-    //     //         count = 1;
-    //     //     }
-    //     //
-    //     //     alert( resultArray );
-    //     // }
-    //
-    // }
-
-    let resultStr = resultArray.join('');
-
-    return resultStr;
+    }
+    return resultArray.join('');
+}
+// нашла на github пример решения...немного переделала
+//https://gist.github.com/simoneb/ce45daa8f5b42b5c88cb
+function rleCompressionWithReduce( str ) {
+   let array = str.split('');
+   let resultStr = array.reduce( ( obj, cur, i, array ) => {
+        if ( i && array[++i] != cur ){//можно и !==???
+            if ( obj.count != 1){//чтобы не выводило 1
+                obj.result += cur + obj.count;
+            } else {
+                obj.result += cur;
+            }
+            obj.count = 1;
+        } else {
+            obj.count++;
+        }
+        return obj;
+   }, { count:1, result:'' } );
+   return resultStr;
 }
 
 alert( rleCompression( str ) );//A3SD3F4A
+alert( rleCompressionWithReduce( str ).result + ' With Reduce' );//A3SD3F4A
